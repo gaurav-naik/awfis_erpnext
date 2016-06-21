df63a75da9d2e26308506db0baa2ed4fcce49bd8
Gaurav Naik <gvrvnk@gmail.com>
2016-06-21 10:34:29 +0530

    Versioning updated

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = '1.0.1'
+import re, ast
+# get version from __version__ variable in awfis_erpnext/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open('awfis_erpnext/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
+
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
