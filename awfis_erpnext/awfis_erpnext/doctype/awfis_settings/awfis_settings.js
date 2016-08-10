393927d41cb2480e719aff11427523ccdf4453ec
Gaurav Naik <gvrvnk@gmail.com>
2016-08-10 15:00:27 +0530

    [Mod] Added confirmation before generating new key.

--- awfis_erpnext/awfis_erpnext/doctype/awfis_settings/awfis_settings.js
+++ awfis_erpnext/awfis_erpnext/doctype/awfis_settings/awfis_settings.js
@@ -7,19 +7,30 @@ frappe.ui.form.on('Awfis Settings', {
 	},
 
 	btn_generate_key_knowlarity: function(frm) {
-		frappe.call({
-			method: "awfis_erpnext.awfis_erpnext.awf.generate_key_knowlarity",
-			freeze: true,
-			freeze_message: __("Generating API key for Knowlarity"),
-			callback: function(r){
-				if(!r.exc) {
-					frm.set_value("api_key_knowlarity", r.message);
-				} else {
-					frappe.msgprint(__("Key was not generated. <br /> " + r.exc));
+		if (frm.doc.api_key_knowlarity) {
+			frappe.confirm("The current key will be replaced. Proceed?", 
+				function() {
+					generate_and_set_key(frm);
 				}
-			}
-		});
+			);
+		} else {
+			generate_and_set_key(frm);
+		}
 	}
-
-
 });
+
+
+function generate_and_set_key(frm) {
+	frappe.call({
+		method: "awfis_erpnext.awfis_erpnext.awf.generate_key_knowlarity",
+		freeze: true,
+		freeze_message: __("Generating API key for Knowlarity"),
+		callback: function(r){
+			if(!r.exc) {
+				frm.set_value("api_key_knowlarity", r.message);
+			} else {
+				frappe.msgprint(__("Key was not generated. <br /> " + r.exc));
+			}
+		}
+	});
+}
