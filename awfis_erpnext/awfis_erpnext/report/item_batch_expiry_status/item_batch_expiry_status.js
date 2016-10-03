e36ea051ecb50ca3033a33f4a4b9aa62f7029b0a
Gaurav Naik <gvrvnk@gmail.com>
2016-10-03 12:22:22 +0530

    [New] Report Item Batch Expiry Status added.

---
+++ awfis_erpnext/awfis_erpnext/report/item_batch_expiry_status/item_batch_expiry_status.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2016, MN Technique and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Item Batch Expiry Status"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"default": sys_defaults.year_start_date,
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"default": frappe.datetime.get_today()
+		}
+	]
+}
